1fc9004752239b4d42034be5cfbcbf2a828b65fe
Mayank12Sharma <sharmaji123mayank@gmail.com>
2025-04-21 23:06:47 +0530

    Intial Commit

---
+++ data/features.js
@@ -0,0 +1,22 @@
+import { BrainCircuit, Briefcase, LineChart, ScrollText } from "lucide-react";
+
+export const features = [
+  {
+    icon: <BrainCircuit className="w-10 h-10 mb-4 text-primary" />,
+    title: "Machine Learning Studio",
+    description:
+      "Get personalized career advice and insights powered by advanced AI technology.",
+  },
+  {
+    icon: <Briefcase className="w-10 h-10 mb-4 text-primary" />,
+    title: "Deep Learning Studio",
+    description:
+      "Practice with role-specific questions and get instant feedback to improve your performance.",
+  },
+  {
+    icon: <LineChart className="w-10 h-10 mb-4 text-primary" />,
+    title: "Data Visualization Studio",
+    description:
+      "Generate, customize, and code any data visualization with AI-powered precision.",
+  },
+];
